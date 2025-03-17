88c83da79d92c7c6033eff22eab35572d74216e6
annacwu <annacwu04@gmail.com>
2025-03-16 22:55:45 -0700

    Finish user info styling

--- App.tsx
+++ App.tsx
@@ -1,18 +1,60 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.topInfo}>
+        <View style={styles.imageColumn}>
+          <View style={styles.photo}/>
+        </View>
+      
+        <View style={styles.userInfoColumn}>
+            <Text style={styles.name}>Anna Wu</Text>
+            <Text style={styles.username}>@banana</Text>
+            <Text style={styles.bio}>Fullstack developer in training!</Text>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    height: '100%',
+  },
+  topInfo: {
+    height: 150,
+    flexDirection: 'row' // this is what makes it go next to each
+  },
+  imageColumn: {
+    height: '100%',
+    width: '25%',
+    justifyContent: 'center', // justify content does y axis (vert)
+    alignItems: 'center' // align does x axis (horizontal)
+  },
+  userInfoColumn: {
+    height: '100%',
+    width: '75%',
     justifyContent: 'center',
   },
+  photo: {
+    height: 80,
+    width: 80,
+    backgroundColor: 'blue',
+    borderRadius: 40, // this is what makes it circular (50 goes in the middle of each side)
+  },
+  name: {
+    fontSize: 20,
+    fontWeight: '500'
+  },
+  username: {
+    fontSize: 16,
+    fontWeight: '400',
+    fontStyle: 'italic'
+  },
+  bio: {
+    marginTop: 5,
+    fontSize: 13,
+    fontWeight: '300'
+  }
 });
