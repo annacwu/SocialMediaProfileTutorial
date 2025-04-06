c1cceb59617eec761a327fe6751a32012c25182e
annacwu <annacwu04@gmail.com>
2025-04-06 16:44:40 -0700

    Rename appentry from root

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,9 @@
 import { Redirect } from 'expo-router';
 import { ROUTES } from "../src/routes";
-import { useAppDispatch } from '../src/store';
-import { PostsActions } from '../src/store/features/posts';
-import { POSTS } from '../src/data/posts';
-import { useEffect } from 'react';
 
-const Root = () => {
-  const dispatch = useAppDispatch();
-
-  useEffect(() => {
-    // dispatch(PostsActions.addPosts(POSTS));
-  }, []);
-
+const AppEntry = () => {
   return <Redirect href={ROUTES.ROOT} />;
 }
 
-export default Root;
+export default AppEntry;
 
